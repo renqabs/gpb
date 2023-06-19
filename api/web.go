476953e39da51c23be7145023eaf5f93e0834f9f
renqabs <130155002+renqabs@users.noreply.github.com>
2023-06-19 10:19:55 +0800

    Update web.go

--- api/web.go
+++ api/web.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"adams549659584/go-proxy-bingai/api/helper"
-	"adams549659584/go-proxy-bingai/common"
-	"adams549659584/go-proxy-bingai/web"
+	"renqabs/gpb/api/helper"
+	"renqabs/gpb/common"
+	"renqabs/gpb/web"
 	"net/http"
 )
 
